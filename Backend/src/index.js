3dbf093dd3bfbdac28159177ddf2431b4ea8c8a5
Yash.B <155563528+YASHUDAYBHAT@users.noreply.github.com>
2025-08-03 16:29:02 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors(
-   { origin:"http://localhost:5173",}
+   { origin:"https://notes-backend-a6de.onrender.com",}
 ));
 app.use(express.json())
 app.use("/api/notes", notesRoute);
